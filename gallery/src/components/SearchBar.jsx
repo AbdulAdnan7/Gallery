c342ebfe2b5c8a16d197b61a70b1d5f1a938a88e
peerabduladnan <peerabduladnan@gmail.com>
2026-03-14 14:01:57 +0530

    Fixed search component

--- gallery/src/components/SearchBar.jsx
+++ gallery/src/components/SearchBar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SearchBar = ({ search, onSearchChange }) => {
   return (
-  
+    <input 
+    type="text"
+    value={search}
+    onChange={onSearchChange}
+    className='border p-2 rounded-xl text-black focus:outline-none focus:ring-2 focus:ring-black'
+    placeholder='Enter author name'
+    />
   )
 }
 
